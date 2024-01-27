4baa235794b64f87a43012bef92f84ebc10fa27b
Víctor Ávila <victoraavila2000@gmail.com>
2024-01-27 13:02:09 -0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Víctor Ávila on 27/01/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
