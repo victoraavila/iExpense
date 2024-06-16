5e1580e72daea33789194e9c1ea545fc5814b79a
Víctor Ávila <victoraavila2000@gmail.com>
2024-06-15 23:57:55 -0300

    Now sorting by name or by amount. Need to readd deleting entries

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -25,72 +25,41 @@ struct ContentView: View {
     
     @State private var expenseTypes = [String]()
     
+    @State private var sortOrder = [SortDescriptor(\ExpenseItem.name)]
+    
     var body: some View {
         NavigationStack() {
             List {
                 Section() {
-                    // Here, using the name as id worked. But we could have multiple expenses with the same name, which sometimes will cause bizarre animations.
-                    // Using the id as id is a much better solution, because we guarantee that there is ~no chance of repeating the same id.
-                    //                ForEach(expenses.items, id: \.id) { item in
-                    //                    Text(item.name)
-                    ForEach(expenses) { item in
-                        if item.type == "Business" {
-                            // Title and subtitle on one side and then more information on the another side
-                            HStack {
-                                VStack(alignment: .leading) {
-                                    Text(item.name)
-                                        .font(.headline)
-                                    
-                                    Text(item.type)
-                                }
-                                
-                                Spacer()
-                                
-                                Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "$"))
-                                    .foregroundStyle(item.amount > 100 ? .red : (item.amount > 10 ? .yellow : .green))
-                            }
-                        }
-                    }
-                    // Adding the functionality to delete (for debug purposes) items via .onDelete()
-                    .onDelete(perform: removeItems)
+                    ExpensesView(expenseType: "Business", sortOrder: sortOrder)
+//                    // Adding the functionality to delete (for debug purposes) items via .onDelete()
+//                    .onDelete(perform: removeItems)
                 } header: {
                     Text("Business expenses")
                 }
                 
 
                 Section() {
-                    // Here, using the name as id worked. But we could have multiple expenses with the same name, which sometimes will cause bizarre animations.
-                    // Using the id as id is a much better solution, because we guarantee that there is ~no chance of repeating the same id.
-                    //                ForEach(expenses.items, id: \.id) { item in
-                    //                    Text(item.name)
-                    ForEach(expenses) { item in
-                        if item.type == "Personal" {
-                            // Title and subtitle on one side and then more information on the another side
-                            HStack {
-                                VStack(alignment: .leading) {
-                                    Text(item.name)
-                                        .font(.headline)
-                                    
-                                    Text(item.type)
-                                }
-                                
-                                Spacer()
-                                
-                                Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "$"))
-                                    .foregroundStyle(item.amount > 100 ? .red : (item.amount > 10 ? .yellow : .green))
-                            }
-                        }
-                    }
+                    ExpensesView(expenseType: "Personal", sortOrder: sortOrder)
                     // Adding the functionality to delete (for debug purposes) items via .onDelete()
-                    .onDelete(perform: removeItems)
+//                    .onDelete(perform: removeItems)
                 } header: {
                     Text("Personal expenses")
                 }
-                
             }
             .navigationTitle("iExpense")
             // Adding the functionality to add (for debug purposes) items via Toolbar
             .toolbar {
+                Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                    Picker("Sort", selection: $sortOrder) {
+                        Text("Sort by Name")
+                            .tag([SortDescriptor(\ExpenseItem.name)])
+                        
+                        Text("Sort by Amount")
+                            .tag([SortDescriptor(\ExpenseItem.amount)])
+                    }
+                }
+                
                 NavigationLink(destination: AddView()) {
                     Button("Add Expense", systemImage: "plus") {} // The label will be used only on VoiceOver
                 }
---
+++ iExpense/ExpensesView.swift
@@ -0,0 +1,55 @@
+//
+//  ExpensesView.swift
+//  iExpense
+//
+//  Created by Víctor Ávila on 15/06/24.
+//
+
+import SwiftData
+import SwiftUI
+
+struct ExpensesView: View {
+    @Query var expenses: [ExpenseItem]
+    @Environment(\.modelContext) var modelContext
+    
+    @State private var expenseType: String
+    
+    var body: some View {
+        ForEach(expenses) { item in
+            if item.type == expenseType {
+                // Title and subtitle on one side and then more information on the another side
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(item.name)
+                            .font(.headline)
+                        
+                        Text(item.type)
+                    }
+                    
+                    Spacer()
+                    
+                    Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "$"))
+                        .foregroundStyle(item.amount > 100 ? .red : (item.amount > 10 ? .yellow : .green))
+                }
+            }
+        }
+        // Adding the functionality to delete (for debug purposes) items via .onDelete()
+//        .onDelete(perform: removeItems)
+    }
+    
+    init(expenseType: String, sortOrder: [SortDescriptor<ExpenseItem>]) {
+        self.expenseType = expenseType
+        _expenses = Query(sort: sortOrder)
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        for i in offsets {
+            modelContext.delete(expenses[i])
+        }
+    }
+}
+
+#Preview {
+    ExpensesView(expenseType: "Business", sortOrder: [SortDescriptor(\ExpenseItem.name)])
+        .modelContainer(for: ExpenseItem.self)
+}
